97333b482c40502ae90c7e9fc8fcafcc41a432d2
Roman Vildanov <vildanovv.rr@gmail.com>
2023-03-18 22:34:20 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import TopPart from './TopPart'
+import BottomPart from './BottomPart'
+
+export const App = () => {
+  return (
+    <div>
+     <TopPart/>
+     <BottomPart/>
+    </div>
+  );
+};
---
+++ src/components/BottomPart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BottomPart = () => {
+    return (
+        <div>
+            <p>777 TWIETERS</p>
+            <p>100.500 FOLLOWERS</p>
+            <button>FOLLOW</button>
+        </div>
+    )
+};
+
+export default BottomPart;
